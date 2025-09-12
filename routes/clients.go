3bb880a849b9067d427fb388ab266ad33bf8ee35
Daniel Acsinte <13287878+Acsigen@users.noreply.github.com>
2025-09-12 16:27:42 +0300

    Implemented DELETE client

--- routes/clients.go
+++ routes/clients.go
@@ -93,3 +93,29 @@ func update_client(context *gin.Context) {
 
 	context.JSON(http.StatusOK, gin.H{"message": "Client updated"})
 }
+
+func delete_client(context *gin.Context) {
+	// Retrieve the path parameter
+	client_id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid client ID"})
+		return
+	}
+
+	client, err := models.GetEventById(client_id)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch client for deletion."})
+		return
+	}
+
+	err = client.Delete(client_id)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not delete client."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Client deleted."})
+
+}
